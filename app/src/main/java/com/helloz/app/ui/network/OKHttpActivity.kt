1a2c9e6e41e10d8d926829e05559be701fd57f16
zzfan <zzf_zefan@qq.com>
2024-09-08 23:15:08 +0800

    Description:添加okhttp示例 Feature or Bugfix:Feature Data:2024年9月8日

--- app/src/main/java/com/helloz/app/ui/network/OKHttpActivity.kt
+++ app/src/main/java/com/helloz/app/ui/network/OKHttpActivity.kt
@@ -2,13 +2,16 @@ package com.helloz.app.ui.network
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
 import androidx.databinding.DataBindingUtil
 import com.helloz.app.R
 import com.helloz.app.base.BaseActivity
 import com.helloz.app.databinding.ActivityOkhttpBinding
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.FormBody
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.Response
 import okio.IOException
 import org.json.JSONObject
 
@@ -32,24 +35,48 @@ class OKHttpActivity : BaseActivity() {
     }
 
     private fun requestBaidu() {
+
+        // 表单数据
+        val formBody = FormBody.Builder().add("test", "这是一个测试参数").build()
+
         // 构建请求
         val request = Request.Builder()
             .url("https://www.baidu.com")
+            .post(formBody)
             .build()
 
-        Thread {
-            try {
+        /*
+                // 同步请求
+                Thread {
+                    try {
+                        // 发送请求，并拿到响应
+                        val response = mOkHttpClient!!.newCall(request).execute()
+
+                        // 打印响应体数据
+                        val responseBody = response.body?.string()
+                        runOnUiThread {
+                            mOkHttpBinding.responseBodyTv.text = responseBody
+                        }
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }.start()
+        */
+        // 异步请求
+        mOkHttpClient?.newCall(request)?.enqueue(object : Callback {
+            override fun onResponse(call: Call, response: Response) {
                 // 发送请求，并拿到响应
                 val response = mOkHttpClient!!.newCall(request).execute()
-
                 // 打印响应体数据
                 val responseBody = response.body?.string()
                 runOnUiThread {
                     mOkHttpBinding.responseBodyTv.text = responseBody
                 }
-            } catch (e: IOException) {
-                e.printStackTrace()
             }
-        }.start()
+
+            override fun onFailure(call: Call, e: java.io.IOException) {
+                Log.d(TAG, "onResponse: failure")
+            }
+        })
     }
 }

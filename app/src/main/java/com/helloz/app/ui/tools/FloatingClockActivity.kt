d8ec1d0dd8b6965bfd8fc1c48551457a0aa0ce7d
zzfan <zzf_zefan@qq.com>
2025-02-22 12:03:02 +0800

    添加悬浮窗功能activity

---
+++ app/src/main/java/com/helloz/app/ui/tools/FloatingClockActivity.kt
@@ -0,0 +1,17 @@
+package com.helloz.app.ui.tools
+
+import android.os.Bundle
+import com.helloz.app.base.BaseActivity
+
+class FloatingClockActivity : BaseActivity() {
+    private val tag: String = this::class.simpleName ?: "FloatingClockActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+
+    }
+}
--- app/src/main/java/com/helloz/app/utils/MenuUtils.kt
+++ app/src/main/java/com/helloz/app/utils/MenuUtils.kt
@@ -8,7 +8,7 @@ package com.helloz.app.utils
 class MenuUtils {
     companion object {
         val MAIN_MENU_LIST_PATH: String = "assets/main_menu_list.xml"
-        val BASIC_TOOLS_MENU_LIST: String = "assets/basis_menu_list.xml"
+        val BASIC_TOOLS_MENU_LIST: String = "assets/basic_tools_menu_list.xml"
         val NETWORK_STRUCTURE_MENU_LIST_PATH: String = "assets/network_structure_menu_list.xml"
         val ITEM: String = "item"
         val TITLE: String = "title"
